79c0e4344db73be8c4c45014f566c06ef340f163
Mzlh2 <shrutisoumya456@gmail.com>
2026-01-14 10:51:13 +0530

    first commit

---
+++ js day2/script.js
@@ -0,0 +1,15 @@
+//map method
+let arr = [1,2,3,4,5]
+
+let result = arr.map((x)=>x)
+console.log(result);
+//filter method
+//let score = [12,7,22,15,32,6,4,27,11,16];
+//let highscores=score.filter()
+//reduce method
+//let numbers = [1,2,3,4,5]
+//let sum=0;
+//for (let i = 0; i < numbers.length; i++) {
+//    sum=sum+numbers[i]
+//}
+let sum=numbers.reduce((acc,curr)=>acc+cu)
