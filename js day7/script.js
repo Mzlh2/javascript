a34c12f907bfa56347d6f027f3409ad224178348
Mzlh2 <shrutisoumya456@gmail.com>
2026-01-28 10:53:59 +0530

    day7

---
+++ js day7/script.js
@@ -0,0 +1,41 @@
+
+// let user={
+//     fullname:"Shruti Soumya",
+//     address:{
+//         city:"gurugram",
+//         state:"haryana"
+//     },
+//     mobileno:"1234567890",
+//     favcolor:["lavender","peach"],
+//     demo:function(){
+//         return "demo function"
+//     }
+// }   
+//console.log(user);
+//console.log(user.fullname,user.mobileno,user.favcolor[1],user.address.city,user.demo(),user.demo);
+
+//console.log("My name is "+user.fullname+". My fav color is "+user.favcolor[0]);
+
+//string literal
+//console.log(`My name is ${user.fullname}. My fav color is ${user.favcolor[0]}`);
+//object methods
+
+//Object.keys(variable name)
+
+// console.log(Object.keys(user));
+
+// console.log(Object.values(user));  //returns array
+
+// console.log(Object.entries(user)); //2-d array
+
+// const car = {
+//     make: "Mahindra",
+//     model: "Thar"
+// };
+//Object.freeze(car); //we can't add a new key and value pair and we can't change or update the existing value
+
+//Object.seal(car); //we can't add a new key and pair value but we can change and update the existing value
+// car.model = "XUV700";
+
+// car.color="stealth black";
+// console.log(car);
